c25be47a995ee74a0f644ff7540d2e0416d64339
Muhammad Ahsan <137956616+Muhammad-Ahsan123@users.noreply.github.com>
2025-01-26 23:26:17 +0500

    fix scroll icon

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,32 @@ import {
   Works,
   Hero,
 } from "./components";
+// import { BiChevronDown } from "react-icons/bi";
+// import { motion } from "framer-motion";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        {/* <div className="fixed top-[92vh] right-4 w-full flex justify-end items-center z-10">
+          <a href="#about">
+            <div className="w-[40px] h-[40px] rounded-3xl border-2 border-white">
+              <motion.div
+                animate={{
+                  y: [0, 10, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="flex flex-col items-center"
+              >
+                <BiChevronDown size={24} className="text-white mb-3" />
+              </motion.div>
+            </div>
+          </a>
+        </div> */}
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@
 // import React from "react";
 // import { ComputersCanvas } from "./canvas";
 // import { styles } from "../styles";
+// import { BiChevronDown } from "react-icons/bi";
 
 // const Hero = () => {
 //   return (
@@ -31,6 +32,25 @@
 //       </div>
 //       {/* <ComputersCanvas /> */}
 
+//       {/* <div className="absolute xs:bottom-3 bottom-20 right-4 w-full flex justify-end items-center">
+//         <a href="#about">
+//           <div className="w-[40px] h-[40px] rounded-3xl border-2 border-white">
+//             <motion.div
+//               animate={{
+//                 y: [0, 10, 0],
+//               }}
+//               transition={{
+//                 duration: 1.5,
+//                 repeat: Infinity,
+//                 repeatType: "loop",
+//               }}
+//               className="flex flex-col items-center"
+//             >
+//               <BiChevronDown size={24} className="text-white mb-3" />
+//             </motion.div>
+//           </div>
+//         </a>
+//       </div> */}
 //       <div className="absolute xs:bottom-3 bottom-20 w-full flex justify-center items-center">
 //         <a href="#about">
 //           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center item-start p-2">
@@ -54,10 +74,12 @@
 
 // export default Hero;
 
+// fkadslfkslaf
 // import { motion } from "framer-motion";
 // import React from "react";
 // import { styles } from "../styles";
 // import profilepic from "../../src/assets/pic.png";
+// import { BiChevronDown } from "react-icons/bi";
 
 // const Hero = () => {
 //   return (
@@ -65,45 +87,43 @@
 //       <div
 //         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 `}
 //       >
-
 //         <div className="flex justify-between max-md:flex-col max-md:gap-8 max-lg:justify-around w-full flex-wrap  lg:flex-row max-md:items-center">
-
-//        <div className="flex">
-//        <div className="flex flex-col justify-start items-center mt-5 mr-2">
-//           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-//           <div className="w-1 sm:h-70 h-60 violet-gradient" />
-//         </div>
-//           <div className="border-white flex flex-1 flex-col">
-//             <h1 className={`${styles.heroHeadText} text-white`}>
-//               Hi, I'm <span className="text-[#915eff]">Ahsan</span>
-//             </h1>
-//             <p
-//               className={`${styles.heroSubText} !leading-6 mt-2 text-white-100`}
-//             >
-//               A creative frontend developer
-//               <br className="sm:block hidden" />
-//               passionate about designing and building
-//               <br className="sm:block hidden" /> user-friendly websites and
-//               applications.
-//             </p>
-//             <div className="flex gap-3 mt-12">
-//               <button
-//                 type="submit"
-//                 className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
+//           <div className="flex">
+//             <div className="flex flex-col justify-start items-center mt-5 mr-2">
+//               <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+//               <div className="w-1 sm:h-70 h-60 violet-gradient" />
+//             </div>
+//             <div className="border-white flex flex-1 flex-col">
+//               <h1 className={`${styles.heroHeadText} text-white`}>
+//                 Hi, I'm <span className="text-[#915eff]">Ahsan</span>
+//               </h1>
+//               <p
+//                 className={`${styles.heroSubText} !leading-6 mt-2 text-white-100`}
 //               >
-//                 <a href="../../public/Ahsan.pdf" download="Ahsan_Resume.pdf">
-//                   Resume
-//                 </a>
-//               </button>
-//               <button
-//                 type="submit"
-//                 className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
-//               >
-//                 <a href="#contact">Hire me</a>
-//               </button>
+//                 A creative frontend developer
+//                 <br className="sm:block hidden" />
+//                 passionate about designing and building
+//                 <br className="sm:block hidden" /> user-friendly websites and
+//                 applications.
+//               </p>
+//               <div className="flex gap-3 mt-12">
+//                 <button
+//                   type="submit"
+//                   className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
+//                 >
+//                   <a href="../../public/Ahsan.pdf" download="Ahsan_Resume.pdf">
+//                     Resume
+//                   </a>
+//                 </button>
+//                 <button
+//                   type="submit"
+//                   className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
+//                 >
+//                   <a href="#contact">Hire me</a>
+//                 </button>
+//               </div>
 //             </div>
 //           </div>
-//        </div>
 
 //           <div className="relative flex justify-center items-center h-[360px] w-[360px] max-md:w-[260px] max-md:h-[260px] bg-gradient-to-r from-[#1e1746] to-[#151030] rounded-full shadow-lg shadow-[#915eff]/30">
 //             {/* Decorative Animated Rings */}
@@ -121,7 +141,7 @@
 //       </div>
 //       {/* <ComputersCanvas /> */}
 
-//       <div className="absolute bottom-8 w-full flex justify-center items-center z-10">
+//       {/* <div className="absolute bottom-8 w-full flex justify-center items-center z-10">
 //         <a href="#about">
 //           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center item-start p-2">
 //             <motion.dev
@@ -135,7 +155,98 @@
 //               }}
 //               className="w-3 h-3 rounded-full bg-white mb-1"
 //             />
-//             {/* arrow icon  */}
+//           </div>
+//         </a>
+//       </div> */}
+//     </section>
+//   );
+// };
+
+// export default Hero;
+
+// import { motion } from "framer-motion";
+// import React from "react";
+// // import { ChevronDown } from "lucide-react"; // Importing the arrow icon
+// import { styles } from "../styles";
+// import profilepic from "../../src/assets/pic.png";
+// import { BiChevronDown } from "react-icons/bi";
+
+// const Hero = () => {
+//   return (
+//     <section className="relative w-full h-screen mx-auto max-md:mb-[80px] border">
+//       <div
+//         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 border-2`}
+//       >
+//         <div className="flex justify-between max-md:flex-col max-md:gap-8 max-lg:justify-around w-full flex-wrap lg:flex-row max-md:items-center">
+//           <div className="flex">
+//             <div className="flex flex-col justify-start items-center mt-5 mr-2">
+//               <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+//               <div className="w-1 sm:h-70 h-60 violet-gradient" />
+//             </div>
+//             <div className="border-white flex flex-1 flex-col">
+//               <h1 className={`${styles.heroHeadText} text-white`}>
+//                 Hi, I'm <span className="text-[#915eff]">Ahsan</span>
+//               </h1>
+//               <p
+//                 className={`${styles.heroSubText} !leading-6 mt-2 text-white-100`}
+//               >
+//                 A creative frontend developer passionate&nbsp;
+//                 <br className="sm:block hidden" />
+//                 about designing and building
+//                 <br className="sm:block hidden" />
+//                 user-friendly websites and applications.
+//               </p>
+//               <div className="flex gap-3 mt-12">
+//                 <button
+//                   type="submit"
+//                   className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
+//                 >
+//                   <a href="../../public/Ahsan.pdf" download="Ahsan_Resume.pdf">
+//                     Resume
+//                   </a>
+//                 </button>
+//                 <button
+//                   type="submit"
+//                   className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
+//                 >
+//                   <a href="#contact">Hire me</a>
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+
+//           <div className="relative flex justify-center items-center h-[360px] w-[360px] max-md:w-[260px] max-md:h-[260px] bg-gradient-to-r from-[#1e1746] to-[#151030] rounded-full shadow-lg shadow-[#915eff]/30">
+//             {/* Decorative Animated Rings */}
+//             <div className="absolute h-full w-full rounded-full bg-gradient-to-r from-[#915eff]/40 to-[#1e1746]/10 animate-pulse" />
+//             <div className="absolute h-[300px] w-[300px] rounded-full border-2 border-[#915eff]/50 blur-lg" />
+
+//             {/* Profile Image */}
+//             <img
+//               src={profilepic}
+//               alt="Profile"
+//               className="z-10 rounded-full w-[320px] h-[320px] max-md:w-[220px] max-md:h-[220px] object-contain shadow-[#915eff]"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//       {/* <ComputersCanvas /> */}
+
+//       <div className="absolute bottom-4 right-4 w-full flex justify-end items-center z-10">
+//         <a href="#about">
+//           <div className="w-[40px] h-[40px] rounded-3xl border-2 border-white flex justify-center items-center">
+//             <motion.div
+//               animate={{
+//                 y: [0, 10, 0],
+//               }}
+//               transition={{
+//                 duration: 1.5,
+//                 repeat: Infinity,
+//                 repeatType: "loop",
+//               }}
+//               className="flex flex-col items-center"
+//             >
+//               <BiChevronDown size={24} className="text-white mb-3" />
+//             </motion.div>
 //           </div>
 //         </a>
 //       </div>
@@ -147,18 +258,17 @@
 
 import { motion } from "framer-motion";
 import React from "react";
-// import { ChevronDown } from "lucide-react"; // Importing the arrow icon
 import { styles } from "../styles";
 import profilepic from "../../src/assets/pic.png";
 import { BiChevronDown } from "react-icons/bi";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto max-md:mb-[80px]">
+    <section className="relative w-full h-screen overflow-hidden">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex justify-between max-md:flex-col max-md:gap-8 max-lg:justify-around w-full flex-wrap lg:flex-row max-md:items-center">
+        <div className="flex justify-between max-md:flex-col max-md:gap-0 max-lg:justify-around w-full flex-wrap lg:flex-row max-md:items-center">
           <div className="flex">
             <div className="flex flex-col justify-start items-center mt-5 mr-2">
               <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -177,7 +287,7 @@ const Hero = () => {
                 <br className="sm:block hidden" />
                 user-friendly websites and applications.
               </p>
-              <div className="flex gap-3 mt-12">
+              <div className="flex gap-3 mt-6">
                 <button
                   type="submit"
                   className="hover:bg-[#1e1746] bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl transition-all duration-300 ease-in-out xs:text-[14px] text-[11px]"
@@ -197,11 +307,9 @@ const Hero = () => {
           </div>
 
           <div className="relative flex justify-center items-center h-[360px] w-[360px] max-md:w-[260px] max-md:h-[260px] bg-gradient-to-r from-[#1e1746] to-[#151030] rounded-full shadow-lg shadow-[#915eff]/30">
-            {/* Decorative Animated Rings */}
             <div className="absolute h-full w-full rounded-full bg-gradient-to-r from-[#915eff]/40 to-[#1e1746]/10 animate-pulse" />
             <div className="absolute h-[300px] w-[300px] rounded-full border-2 border-[#915eff]/50 blur-lg" />
 
-            {/* Profile Image */}
             <img
               src={profilepic}
               alt="Profile"
@@ -210,11 +318,10 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      {/* <ComputersCanvas /> */}
 
-      <div className="absolute bottom-4 right-4 w-full flex justify-end items-center z-10">
+      <div className="absolute xs:bottom-3 bottom-20 right-4 w-full flex justify-end items-center">
         <a href="#about">
-          <div className="w-[40px] h-[40px] rounded-3xl border-2 border-white flex justify-center items-center">
+          <div className="w-[40px] h-[40px] rounded-3xl border-2 border-white">
             <motion.div
               animate={{
                 y: [0, 10, 0],
